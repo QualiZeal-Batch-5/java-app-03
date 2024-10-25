0309e7a8ee7792fc10a7df6d4146e1b719a1e940
Kattupalli Bhushana kumari <bhushana1805@gmail.com>
2024-10-25 10:17:34 +0530

    Performed subtraction operation.

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,11 @@
-package com.qualizeal;
+package com.qualizeal.javaapp03;
+
+import com.qualizeal.javaapp03.operation.TwoNonDecimalNumbersSubtraction;
 
 public class App {
 	public static void main(String[] args) {
-		
+		TwoNonDecimalNumbersSubtraction twoNonDecimalNumbersSubtraction = new TwoNonDecimalNumbersSubtraction();
+		int result1 = twoNonDecimalNumbersSubtraction.subtract(12,20);
+		System.out.println(result1);
 	}
 }
