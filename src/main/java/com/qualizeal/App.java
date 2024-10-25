af17520a2579ba7e209db4b6a0baa77aae269dd0
Shravani <sravanig066@gmail.com>
2024-10-25 10:55:02 +0530

    Subtraction.

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,12 @@
 package com.qualizeal;
+import com.qualizeal.javaapp03.operation.TwoNonDecimalNumbersSubtraction;
 
 public class App {
-	public static void main(String[] args) {
-		
-	}
+      public static void main(String[] args) {
+            TwoNonDecimalNumbersSubtraction twoNonDecimalNumbersSubtraction = new TwoNonDecimalNumbersSubtraction();
+            System.out.println(twoNonDecimalNumbersSubtraction.subtract(10,2));
+            System.out.println(twoNonDecimalNumbersSubtraction.subtract(2,8));
+            
+            
+      }
 }
