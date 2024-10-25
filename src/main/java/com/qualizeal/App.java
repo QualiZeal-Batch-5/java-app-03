e4085b28022a1552e54cb5aa9cbb875f63e241f2
Dandu Srija <dandusrija2503@gmail.com>
2024-10-25 10:49:51 +0530

    Performed subtraction operation on two non-decimal numbers.

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,15 @@
 package com.qualizeal;
 
+import com.qualizeal.javaapp03.operation.TwoNonDecimalNumbersSubtraction;
+
 public class App {
 	public static void main(String[] args) {
 		
+		TwoNonDecimalNumbersSubtraction TwoNonDecimalNumbersSubtraction = new TwoNonDecimalNumbersSubtraction();
+		int result = TwoNonDecimalNumbersSubtraction.subtract(10,2); 
+		System.out.println(result);
+		
 	}
 }
+
+
