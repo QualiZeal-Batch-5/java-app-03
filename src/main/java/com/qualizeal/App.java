0b8412d684c9ccb248df4e4661cbaee32592b471
varsha ch <varshachittaboina@gmail.com>
2024-10-25 10:11:19 +0530

    assignment-3 added

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,11 @@
-package com.qualizeal;
-
+package com.qualizeal.javaapp03;
+import com.qualizeal.javaapp03.operation.TwoNonDecimalNumbersSubtraction;
 public class App {
 	public static void main(String[] args) {
-		
+		TwoNonDecimalNumbersSubtraction twoNonDecimalNumbersSubtraction = new TwoNonDecimalNumbersSubtraction();
+		int result1 = twoNonDecimalNumbersSubtraction.subtract(10,3);
+		System.out.println(result1);
+		int result2 = twoNonDecimalNumbersSubtraction.subtract(10,4);
+		System.out.println(result2);
 	}
 }
