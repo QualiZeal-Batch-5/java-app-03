c670e367ac9bc2c80ae8487ff9502adc1c9e3cf9
Chandana Grandhi <grandhichandana4830@gmail.com>
2024-10-25 10:33:16 +0530

    addingfiles

---
+++ src/main/java/App.java
@@ -0,0 +1,15 @@
+package com.qualizeal.javaapp03.operation;
+
+import com.qualizeal.javaapp03.operation.TwoNonDecimalNumbersSubtraction;
+
+public class App{
+	public static void main(String[] args){
+		
+		TwoNonDecimalNumbersSubtraction twoNonDecimalNumbersSubtraction =  new TwoNonDecimalNumbersSubtraction();
+		
+		int result1 = twoNonDecimalNumbersSubtraction.subtract(10,2);
+		
+		System.out.println(result1);
+		
+	}
+}
