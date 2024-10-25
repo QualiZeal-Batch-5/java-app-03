8106504f6ddd33fad519bcc08c77974465b0e30b
Sri Harika <nookalasriharika@gmail.com>
2024-10-25 10:35:53 +0530

    Subtraction

---
+++ src/main/java/TwoNonDecimalNumbersSubtraction.java
@@ -0,0 +1,14 @@
+package com.qualizeal.javaapp03.operation;
+public class TwoNonDecimalNumbersSubtraction {
+public long subtract(int a,int b) {
+int result = 0;
+if(a<0 || b<0) {
+result = -1; }
+else if(a == 0 || b == 0) {
+result = -2;
+} else {
+result = a - b; 
+}
+return result;
+}
+}
