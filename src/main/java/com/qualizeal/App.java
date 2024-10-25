d0540683ceaddecda5e11d4cb1935de2e23c0e1d
vijaya lakshmi <dasarisaibalaji2006@gmail.com>
2024-10-25 10:12:37 +0530

    Solution added

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,7 +1,13 @@
 package com.qualizeal;
+import com.qualizeal.javaapp03.operation.TwoNonDecimalNumbersSubtraction;
 
 public class App {
 	public static void main(String[] args) {
+		TwoNonDecimalNumbersSubtraction twoNonDecimalNumbersSubtraction = new TwoNonDecimalNumbersSubtraction();
+		int result = twoNonDecimalNumbersSubtraction.subtract(10,2);
+		System.out.println(result);
+		
 		
 	}
 }
+
