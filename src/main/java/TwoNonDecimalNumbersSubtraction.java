ba8b4e3691c2bac0e6f243424c84fd51738bf0d3
( sura deepika) <( suradeepika2515@gmail.com)>
2024-10-25 10:56:49 +0530

    solution

---
+++ src/main/java/TwoNonDecimalNumbersSubtraction.java
@@ -0,0 +1,17 @@
+package com.qualizeal.javaapp03.operation;
+ 
+public class TwoNonDecimalNumbersSubtraction{
+	public int subtract(int num1 , int num2){
+		int result = 0;
+		if (num1 < 0 || num2 < 0){
+			return -1;
+		}
+		else if (num1 == 0 || num2 == 0){
+			return -2;
+		}
+		else {
+			result = num1 - num2;
+		}
+		return result;
+	}
+}
