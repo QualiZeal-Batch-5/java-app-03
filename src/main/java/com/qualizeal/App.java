9dade48541099e63b6e84ab3ffea00dce503b51c
PriyankaVeeranala <priyankanaidu2003@gmail.com>
2024-11-09 16:15:59 +0530

    File uploaded

--- src/main/java/com/qualizeal/App.java
+++ src/main/java/com/qualizeal/App.java
@@ -1,9 +1,17 @@
+package com.qualizeal;
+ 
 import com.qualizeal.javaapp03.operation.TwoNonDecimalNumbersSubtraction;
-
+ 
 public class App {
-    public static void main(String[] args) {
-        TwoNonDecimalNumbersSubtraction twoNonDecimalNumbersSubtraction = new TwoNonDecimalNumbersSubtraction();
-        
-        twoNonDecimalNumbersSubtraction.subtract(10, 2); 
-    }
+
+	public static void main(String[] args) {
+
+		TwoNonDecimalNumbersSubtraction twoNonDecimalNumbersSubtraction = new TwoNonDecimalNumbersSubtraction();
+
+		int result1 = twoNonDecimalNumbersSubtraction.subtract(10,2);
+
+		System.out.println(result1);
+
+	}
+
 }
